71f601276d755b1387b72902416416ef00f19e29
Rajiv singh <49805637+RajivsinghRocks@users.noreply.github.com>
2022-10-08 02:09:26 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*=================================Typing Animation =====================*/
+var typed =new Typed(".typing",{
+    strings:[ " ","Web Designer","UI/UX Designer","Software Tester"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
